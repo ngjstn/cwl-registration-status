a2be05d05873d2da1c121596d48ea35563551349
Justin Ng <ngjustin2002@gmail.com>
2022-06-30 01:30:03 -0700

    class definitions

--- cwlscraper.py
+++ cwlscraper.py
@@ -1,20 +1,71 @@
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException 
+from selenium import webdriver 
+from email.message import EmailMessage
+import smtplib 
+import yaml
+import time 
+import datetime 
 import yaml 
+
 
 class CWLscraper(object): 
     def __init__(self): 
-        with open('config.yaml', 'r') as file: 
-            print('Parsing config.yaml')
-            props = yaml.safe_load(file)
+        with open('config.yaml') as file: 
+            try: 
+                self.props = yaml.safe_load(file)
+            except yaml.YAMLError as e: 
+                print('ERROR: %s' % e)
+        # with webdriver.Chrome(executable_path=self.props['driver']['exec_path']) as web_drive:
+        #     self.driver = web_drive 
+        #     self.driver = webdriver.Chrome(executable_path=self.props['driver']['exec_path'])
+        #     self.driver.get(self.props['driver']['scrape_dest'])
+        #     self.driver.set_window_size(1080, 800)
+        #     print('CWLscraper object created')
+        try: 
+            self.driver = webdriver.Chrome(executable_path=self.props['driver']['exec_path'])
+            self.driver.get(self.props['driver']['scrape_dest'])
+            self.driver.set_window_size(1080, 800)
+            print('CWLscraper object created')
+        except Exception as e: 
+            print('ERROR: %s' % e)
 
-        self.number = props['user_contact']['phone_number']
-        self.carrier = props['user_contact']['carrier'] 
+    def cwl_login(self): 
+        print('Navigating to CWL Login form...')
+        self.driver.find_element(By.XPATH, "//input[@name = 'IMGSUBMIT']").click()
+        print('Current Page: %s' % self.driver.title) 
 
+        form = self.driver.find_element(By.XPATH, "//form[@id = 'fm1']")
+        print('Inputting login credentials')
+        form.find_element(By.NAME, 'username').send_keys(scraper.props['cwl']['username'])
+        form.find_element(By.NAME, 'password').send_keys(scraper.props['cwl']['password'])
 
-    def list_props(self): 
-        print('number: %s' % self.number)
-        print('carrier: %s' % self.carrier)
+        print('Submitting Form')
+        scraper.driver.find_element(By.XPATH, "//input[@type = 'submit']").click()
+
+        print('Current Page: %s' % self.driver.title)
+
+        # need to change this assertion for invalid logins 
+        if 'Your account is inactive' in self.driver.title: 
+            print('ERROR: Failed to login. Check your CWL credentials')
+            return False 
+        else: 
+            print('Login successful') 
+            return True 
+
+    
+    # def __enter__(self): 
+    #     print('Enter') 
+
+    # def __exit__(self, exc_type, exc_val, exc_tb): 
+    #     print('Exit')
+    #     self.driver.quit() 
 
 
 if __name__ == '__main__': 
-    auto = Scraper()
-    auto.list_props()
+    scraper = CWLscraper() 
+    scraper.cwl_login()
+    scraper.driver.close()
+    # with CWLscraper() as scraper: 
+    #     scraper.cwl_login()
+    
